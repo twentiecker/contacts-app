15c88a0c0ce3aeea80349ed4f338117495ea54d8
twentiecker <twentiecker@gmail.com>
2022-06-21 13:06:29 +0700

    fix getData contacts

--- src/ContactApp.js
+++ src/ContactApp.js
@@ -3,7 +3,7 @@ import ContactList from "./ContactList";
 import { getData } from "./data";
 
 function ContactApp() {
-  const contacts = getData;
+  const contacts = getData();
   return (
     <div className="contact-app">
       <h1>Daftar Kontak</h1>
