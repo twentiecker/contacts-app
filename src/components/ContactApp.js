7c6b8b7c10c529ced3b8cdefab255568e3b5441d
twentiecker <twentiecker@gmail.com>
2022-06-22 09:56:21 +0700

    finalizing app

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
+import ContactInput from "./ContactInput";
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -37,7 +38,10 @@ class ContactApp extends React.Component {
   render() {
     return (
       <div className="contact-app">
-        <h1>Daftar Kontak</h1>
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
+        <h2>Daftar Kontak</h2>
         <ContactList
           contacts={this.state.contacts}
           onDelete={this.onDeleteHandler}
